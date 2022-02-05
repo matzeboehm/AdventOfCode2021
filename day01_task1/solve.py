11838f0ea425d8e7f875c2f2776f23d026ac01ea
Matthias Boehm <matthias1.boehm@web.de>
2022-02-05 21:53:53 +0100

    Day 1 Task 1 solved - update

--- day01_task1/solve.py
+++ day01_task1/solve.py
@@ -6,7 +6,5 @@ with open("/Users/matthiasboehm/Documents/AdventOfCode2021/day01_task1/input.txt
         if index < len(lines) - 1:
             if lines[index + 1] > line:
                 counter += 1
-                print(line)
-                print(lines[index +1])
 
     print(counter)
