4a2805c30e60ed51c292364079fd4c334520b89f
Matthias Boehm <matthias1.boehm@web.de>
2022-02-07 08:42:37 +0100

    solved first part of day2

---
+++ day02/solve_01.py
@@ -0,0 +1,17 @@
+with open("/Users/matthiasboehm/Documents/AdventOfCode2021/day02/input.txt", "r") as file:
+    lines = file.readlines()
+
+    horizontal = 0
+    vertical = 0
+
+    for i in lines:
+        a = i.split()
+
+        if a[0] == "forward":
+            horizontal += int(a[1])
+        elif a[0] == "up":
+            vertical -= int(a[1])
+        elif a[0] == "down":
+            vertical += int(a[1])
+
+    print(horizontal*vertical)
