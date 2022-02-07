20bd315005cf2374d2c97bd304980c5f0487645b
Matthias Boehm <matthias1.boehm@web.de>
2022-02-07 09:22:29 +0100

    solved task 1 of day 3

---
+++ day03/solve_01.py
@@ -0,0 +1,25 @@
+with open("/Users/matthiasboehm/Documents/AdventOfCode2021/day03/input.txt", "r") as file:
+    lines = file.readlines()
+
+    string = [str(i).strip() for i in lines]
+    length = len(string[0])
+
+    gamma = ""
+    epsilon = ""
+
+    for i in range(length):
+        temp = ""
+        for line in string:
+            temp += line[i]
+        
+        zeros = temp.count("0")
+        ones = len(temp) - zeros
+
+        if zeros > ones:
+            gamma += "0"
+            epsilon += "1"
+        elif ones > zeros:
+            gamma += "1"
+            epsilon += "0"
+
+    print(int(gamma, 2) * int(epsilon, 2))
