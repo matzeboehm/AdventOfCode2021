f3fda20dc647365a7933a8f7c67699644589ddd5
Matthias Boehm <matthias1.boehm@web.de>
2022-02-07 17:31:36 +0100

    solved task 2 on day 4

---
+++ day04/solve_02.py
@@ -0,0 +1,39 @@
+from subprocess import call
+import numpy as np
+
+with open("/Users/matthiasboehm/Documents/AdventOfCode2021/day04/input.txt", "r") as file:
+    lines = file.readlines()
+
+    callout = lines[0].replace("\n", "").split(",")
+    callout = [int(i) for i in callout]
+    
+    numbers = lines[2:]
+    numbers = [i.replace("\n", "") for i in numbers]
+    
+    sol = []
+    longestSolve = []
+
+    for i in range(int((len(numbers) + 1) /6)):
+        temp = np.array([
+                    numbers[i*6].split(), 
+                    numbers[i*6+1].split(),
+                    numbers[i*6+2].split(),
+                    numbers[i*6+3].split(),
+                    numbers[i*6+4].split()])
+        temp = temp.astype(int)
+
+        for index, caller in enumerate(callout):
+            # replace number called with -1
+            temp = np.where(temp == caller, -1, temp)
+
+            # check if the one row or one column has only -5, if so BINGO
+            # for sum, those values must be counted as 0
+            if -5 in temp.sum(axis=0) or -5 in temp.sum(axis=1):
+                longestSolve.append(index)
+                temp = np.where(temp == -1, 0, temp)
+                sol.append(np.sum(temp) * caller)
+                break
+            
+    slowestIndex = max(longestSolve)
+    sol_index = longestSolve.index(slowestIndex)
+    print(sol[sol_index])
