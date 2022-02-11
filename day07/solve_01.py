0f08f9505d289b91c0e01d4521565eb60d162660
Matthias Boehm <matthias1.boehm@web.de>
2022-02-11 10:07:03 +0100

    day 7 solved task 1

---
+++ day07/solve_01.py
@@ -0,0 +1,16 @@
+with open("input.txt", "r") as file:
+    input = file.readline().split(",")
+    input = list(map(int, input))
+
+    min_val = min(input)
+    max_val = max(input)
+
+    sol = {}
+
+    for i in range(min_val, max_val + 1):
+        temp = [abs(value - i) for value in input]
+        sol[i] = sum(temp)
+
+    min_fuel_key = min(sol, key=sol.get)
+    min_fuel = sol[min_fuel_key]
+    print("Minimum fuel needed: %i" %(min_fuel))
