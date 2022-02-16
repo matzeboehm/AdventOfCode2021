747902ab061f026031a7a047fee73b70d1727237
Matthias Boehm <matthias1.boehm@web.de>
2022-02-16 15:52:01 +0100

    task 1 and task 2 solved day 11

--- day11/solve_01.py
+++ day11/solve_01.py
@@ -1,4 +1,6 @@
+from curses import flash
 import numpy as np
+
 with open("input.txt", "r") as file:
     lines = file.readlines()
 
@@ -7,49 +9,56 @@ with open("input.txt", "r") as file:
 
     input = np.zeros((rows, cols), int)
     ones = np.ones((rows, cols), int)
-
-    steps = 100
-
-    totalFlashes = 0
-
-    for i, row in enumerate(lines):
-        for j, val in enumerate(row.strip()):
-            input[i][j] = int(val)
     
-    whereToAdd = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
+    # Convert string to numpy array
+    for i, line in enumerate(lines):
+        for j, val in enumerate(line.strip()):
+            input[i][j] = int(val)
+
+    # steps = 100
+    steps = 250
+    flashCounter = 0
+    brightestFlash = []
+
+    neighbors = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
+
     for step in range(steps):
         input += ones
-        flashed = []
+        flashesThisStep = []
 
-        while np.amax(input) >= 10:    
+        # while max value in array greater than 10
+        while np.amax(input) >= 10:
+            paddedInput = np.pad(input, 1, mode="constant", constant_values=0)
             adder = np.zeros((rows+2, cols+2), int)
-            paddedInput = np.pad(input, 1, mode = "constant", constant_values=0)
 
-            for i in range(1, rows + 2):
-                for j in range(1, cols + 2):
-                    if ((i,j)) not in flashed and paddedInput[i][j] >= 10:
-                        flashed.append((i,j))
-                        totalFlashes += 1
+            for row in range(1, rows+1):
+                for col in range(1, cols+1):
+                    if paddedInput[row][col] >= 10 and ((row,col)) not in flashesThisStep:
+                        flashesThisStep.append((row,col))
+                        flashCounter += 1
 
-                        for k in whereToAdd:
-                            x,y = k
-                            row_adder = i + x
-                            col_adder = j + y
+                        for neighbor in neighbors:
+                            deltaRow, deltaCol = neighbor
+                            rowCheck = row + deltaRow
+                            colCheck = col + deltaCol
 
-                            if (0 < row_adder < (rows + 2)) and (0 < col_adder < (cols + 2)) and ((row_adder, col_adder)) not in flashed:
-                                adder[row_adder][col_adder] += 1
+                            adder[rowCheck][colCheck] += 1
 
-            for flash in flashed:
-                x,y = flash
+            paddedInput += adder
+
+            # set all flashed values to 0
+            for pos in flashesThisStep:
+                x,y = pos
                 paddedInput[x][y] = 0
-            
+
             input = paddedInput[1:-1, 1:-1]
 
-            adder = adder[1:-1, 1:-1]
-            input += adder
-            
-        print("Step %i" %(step + 1))   
+        # all fishes bright up 
+        if np.sum(input) == 0:
+            brightestFlash.append(step + 1)
+
+        print("After Step %i" %(step + 1))
         print(input)
-    print("Total flashes %i" %(totalFlashes))
-    # 1755
-            
+
+    print("Flash Counter %i" %(flashCounter))
+    print(sorted(brightestFlash))
