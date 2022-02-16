7c69cdee85ea9594927a68b337be27e988d24bcb
Matthias Boehm <matthias1.boehm@web.de>
2022-02-16 21:40:18 +0100

    day 08 solved task 2

--- day08/solve_02.py
+++ day08/solve_02.py
@@ -1,4 +1,4 @@
-with open("input.txt", "r") as file:
+with open("/Users/matthiasboehm/Documents/AdventOfCode2021/day08/input.txt", "r") as file:
     lines = file.readlines()
     
     zero = "abcefg"   # len 6
@@ -23,78 +23,71 @@ with open("input.txt", "r") as file:
 
         # store with unique length
         lookup = {}
-        len_6 = []
-        len_5 = []
+        len_6 = set()
+        len_5 = set()
         for i in clues:
+            # all strings are sorted alphabetically
             sorted_string = sorted(i)
             string = "".join(sorted_string)
             # unique lengths --> numbers 1, 4, 7, 8
             if len(i) in length_lookup.keys():                
-                lookup[length_lookup[len(i)]] = string
+                if not lookup.get(length_lookup[len(i)]):
+                    lookup[length_lookup[len(i)]] = string
             # All length 6 --> numbers 0, 6, 9
             elif len(i) == len(zero):
                 if string not in len_6:
-                    len_6.append(string)
-            # Aöö length 5 --> numbers 2, 3, 5
+                    len_6.add(string)
+            # All length 5 --> numbers 2, 3, 5
             elif len(i) == len(two):
                 if string not in len_5:
-                    len_5.append(string)
+                    len_5.add(string)
 
-        # aus 1 und 6 kann c bestimmt werden
+        # 1 must contain c and f. c is in 4 out of all len5 and len6, f is in 5 out of all len5 and len6
         code_c = ""
         code_f = ""
-        for i in len_6:
-            one_v = set(lookup.get(1))
-            temp = set(i)
-
-            if len(one_v & temp) == 1:
-                code_f = list(one_v & temp)[0]
-                code_c = "".join(list(one_v)).replace(code_f, "")
-                break
-
+        tempList = list(len_5.union(len_6))
+        
+        one_v = lookup.get(1)
+        counter = 0
+        for i in tempList:
+            if one_v[0] in i:
+                counter += 1
+        if counter == 4:
+            code_c = one_v[0]
+            code_f = one_v[1]
+        elif counter == 5:
+            code_f = one_v[0]
+            code_c = one_v[1]
+        
         for i in len_5:
-            if code_c in i and code_f in i:
-                lookup[3] = i
-            elif code_c in i and code_f not in i:
+            if code_c in i and code_f not in i:
                 lookup[2] = i
+            elif code_c in i and code_f in i:
+                lookup[3] = i
             elif code_c not in i and code_f in i:
                 lookup[5] = i
 
-        code_e = ""
-        one_v = set(lookup.get(1))
-        four_v = set(lookup.get(4))
-        seven_v = set(lookup.get(7))
-        eight_v = set(lookup.get(8))
-        compare = set.union(one_v, four_v, seven_v)
-        for i in "".join(list(eight_v)):
-            if i not in "".join(list(compare)):
-                code_e = i
-                break
-        
+        # in the unique lenghts, e and g only appears in 8. e and g appear together in 0
+        unique_147 = {i for i in (lookup[1] + lookup[4] + lookup[7])}
+        unique_8 = {i for i in lookup[8]}
+        code_eg = list(unique_8.difference(unique_147))
+
         for i in len_6:
-            if code_c not in i and code_f in i:
+            if code_c not in i:
                 lookup[6] = i
-            elif code_c in i and code_e in i:
+            elif all(j in i for j in code_eg):
                 lookup[0] = i
-            elif code_c in i and code_e not in i:
+            else:
                 lookup[9] = i
 
-
-        # in lookup steht welche Kombination (sortiert) gleich welcher Zahl entspricht
+        # invert lookup
         lookup = {y:x for x,y in lookup.items()}
 
-        # Aus Numbers muss jetzt noch umgewandelt werden 
-        string_num = ""
-        print(lookup)
-
-        for i in numbers:
-            sorted_string = sorted(i)
-            string = "".join(sorted_string)
-
-            string_num += str(lookup.get(string))
-
-        int_num = int(string_num)
-        print(int_num)
-        sol += int_num
-    print(sol)
-    # not correct yet, not all values are correct in the lookup
+        string = ""
+        for number in numbers:
+            sorted_number = "".join(sorted(number))
+            string += str(lookup[sorted_number])
+        
+        sol += int(string)
+    
+    print("Sum of all numbers %i" %(sol))
